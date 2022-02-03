8700e911064842f7723f8154e0e2cd6fdb892916
Scott Martain <56328932+SMartain@users.noreply.github.com>
2022-02-03 16:31:01 -0600

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,19 @@
+const toggleTag = document.querySelector("a.toggle-nav")
+const mainTag = document.querySelector("main")
+
+// when I click the toggle tag, toggle a class of open on the main tag
+// and if it's open make the toggle tab say closed
+// and if not make the toggle tag say open
+
+toggleTag.addEventListener("click", function () {
+  mainTag.classList.toggle("open")
+  
+  if (mainTag.classList.contains("open")) {
+    toggleTag.innerHTML = '<img src="close.svg"> Close'
+  } else {
+    toggleTag.innerHTML = '<img src="menu.svg"> Menu'
+  }
+
+
+
+})
